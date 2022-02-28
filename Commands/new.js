55fcfa7c5a35a54fa236acfc352c38b28a4e941f
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2022-02-28 13:20:45 -0500

    Update new.js

--- Commands/new.js
+++ Commands/new.js
@@ -106,6 +106,8 @@ module.exports = {
                             }
                         ]
                     });
+
+                    process.exit()
                 }
                 client.on("interactionCreate", async i => {
                     if(canceled == true) return i.reply(`❌`);
